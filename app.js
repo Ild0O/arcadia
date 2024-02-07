2a342908440e678f3f3a4f6a054e987bc6a411fa
manu garcia <garciaemmanuel@hotmail.com>
2024-02-07 11:28:26 +0100

    add gate img

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ const container = document.querySelector('.container');
 const containerNode = container.append(createTitle);
 const title = document.querySelector('h1');
 
+const imgPath = './public/img/gate.png';
+
+const gateImg = document.querySelector('img');
+gateImg.src = imgPath;
+
+
 title.textContent = "Where she is ?"
