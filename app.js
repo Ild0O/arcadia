23dca1417b10fd48cc0e7ab6b90929f39e7c75c3
manu garcia <garciaemmanuel@hotmail.com>
2024-02-07 11:18:09 +0100

    add title

---
+++ app.js
@@ -0,0 +1,6 @@
+const createTitle = document.createElement('h1');
+const container = document.querySelector('.container');
+const containerNode = container.append(createTitle);
+const title = document.querySelector('h1');
+
+title.textContent = "Where she is ?"
